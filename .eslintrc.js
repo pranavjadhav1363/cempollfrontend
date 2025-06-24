c4eaf1265f14aeb14420b375bba9bd5e71441413
Pranav Jadhav <pranavjadhavworks@gmail.com>
2025-06-24 19:15:03 +0530

    third commit

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: 'next',
-  rules: {
-    'react/no-unescaped-entities': 'off',
-  },
-};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,14 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
